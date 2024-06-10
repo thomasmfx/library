e5ef2e793fe68851643916d512e6a1b50327c48f
Thomas Moisés <thomasmoisesf@gmail.com>
2024-06-09 22:47:02 -0300

    feat: Make parts of the add/remove books feature

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const booksPlaceholders = document.querySelectorAll(".book-placeholder");
 const books = document.querySelectorAll(".book");
 const bookReadIcons = document.querySelectorAll(".book-read-icon");
 const removeBookIcons = document.querySelectorAll(".remove-book-icon");
-
 let bookReadStatus = null;
 
 newBookBtn.addEventListener("click", () => {
@@ -20,9 +19,7 @@ newBookBtn.addEventListener("click", () => {
 
 submitBookBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    formRows.forEach(row => {
-        alert(row.children[1].value)
-    })
+    addBookToLibrary()
 });
 
 cancelBookSubmitBtn.addEventListener("click", () => {
@@ -59,5 +56,64 @@ function Book(name, author, pages, readStatus) {
 }
 
 function addBookToLibrary() {
+    let bookName = document.querySelector("#book-name").value;
+    let bookAuthor = document.querySelector("#book-author").value;
+    let bookPages = document.querySelector("#book-pages").value;
+    let bookIndex = null;
+    
+    if(document.querySelector("#radio-read").checked) {
+        bookReadStatus = true
+    } else {
+        bookReadStatus = false
+    }
 
+    let book = new Book(bookName, bookAuthor, bookPages, bookReadStatus)
+    myLibrary.push(book)
+    for(let i = 0; i < myLibrary.length; i++) {
+        if(myLibrary[i] === book) {
+            bookIndex = i;
+            // alert(myLibrary[i] = `book${i}`)
+        }
+    }
+
+booksPlaceholders.forEach(placeholder => {
+        if(!placeholder.hasChildNodes()) {
+            const createBook = document.createElement("div")
+            createBook.classList.add('book');
+            const createBookSideStripe = document.createElement("div")
+            createBookSideStripe.classList.add('book-side-stripe');
+            const createBookNameBg = document.createElement("div")
+            createBookNameBg.classList.add('book-title-bg');
+            const createBookName = document.createElement("p");
+            createBookName.textContent = bookName;
+            const createBookInfo = document.createElement("div")
+            createBookInfo.classList.add('book-info');
+            const createBookAuthor = document.createElement("p");
+            createBookAuthor.textContent = `by: ${bookAuthor}`;
+            const createBookPages = document.createElement("p");
+            createBookPages.textContent = `${bookPages} pp.`;
+            const createBookIcons = document.createElement("div")
+            createBookIcons.classList.add('book-icons');
+            const createReadIcon = document.createElement("img")
+            createReadIcon.classList.add('book-read-icon');
+            createReadIcon.src = 'assets/images/read_checked.svg'
+            const createRemoveIcon = document.createElement("img")
+            createRemoveIcon.classList.add('remove-book-icon')
+            createRemoveIcon.src = 'assets/images/remove.svg'
+
+            createBookIcons.appendChild(createReadIcon);
+            createBookIcons.appendChild(createRemoveIcon);
+
+            createBookInfo.appendChild(createBookAuthor);
+            createBookInfo.appendChild(createBookPages);
+            
+            createBook.appendChild(createBookSideStripe);
+            createBook.appendChild(createBookNameBg);
+            createBook.appendChild(createBookName)
+            createBook.appendChild(createBookInfo);
+            createBook.appendChild(createBookIcons);
+
+            placeholder.appendChild(createBook)
+        }
+    })
 }
