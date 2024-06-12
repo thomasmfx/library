2e5f7bb4c7892fca6f587e33560a447fd79dee9c
Thomas Moisés <thomasmoisesf@gmail.com>
2024-06-11 21:55:24 -0300

    fix/style: Fix read icon behavior and change elements colors/background

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ submitBookBtn.addEventListener("click", (event) => {
     event.preventDefault();
     let bookName = document.querySelector("#book-name").value;
     let input = document.querySelector("#book-name");
-    
+
     if(bookName !== '') {
         addBookToLibrary()
         input.setCustomValidity("");
@@ -165,3 +165,64 @@ function organizeBooks() {
         }
     })
 };
+
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#49111c"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#49111c",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
